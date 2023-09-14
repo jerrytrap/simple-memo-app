30597edf54c875a170ddd091897dbf0bee393516
류대현 <eogus1027@naver.com>
2023-09-14 13:04:27 +0900

    feat: 메모 목룍 표시 #3

--- app/src/main/java/com/lunchplay/simplememoapp/MainActivity.kt
+++ app/src/main/java/com/lunchplay/simplememoapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.lunchplay.simplememoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.lunchplay.simplememoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val adapter = MemoAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setBinding()
+
+        setRecyclerView()
+    }
+
+    private fun setBinding() {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.activity_main, null, false)
+        binding.lifecycleOwner = this
+        setContentView(binding.root)
+    }
+
+    private fun setRecyclerView() {
+        binding.recyclerViewMemoList.apply {
+            this.adapter = this@MainActivity.adapter
+            this.layoutManager = LinearLayoutManager(this@MainActivity)
+        }
     }
 }
---
+++ app/src/main/java/com/lunchplay/simplememoapp/MemoAdapter.kt
@@ -0,0 +1,46 @@
+package com.lunchplay.simplememoapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.lunchplay.simplememoapp.databinding.ItemMemoBinding
+
+class MemoAdapter : ListAdapter<Memo, RecyclerView.ViewHolder>(diffUtil) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding: ItemMemoBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_memo,
+            parent,
+            false
+        )
+
+        return MemoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as MemoViewHolder).bind(getItem(position))
+    }
+
+    class MemoViewHolder(
+        private val binding: ItemMemoBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(memo: Memo) {
+            binding.memo = memo
+        }
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<Memo>() {
+            override fun areItemsTheSame(oldItem: Memo, newItem: Memo): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Memo, newItem: Memo): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
