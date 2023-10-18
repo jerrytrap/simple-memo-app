cc25ef6a9648c0a33658c77d66c7c8a863cc1504
류대현 <eogus1027@naver.com>
2023-10-19 00:13:12 +0900

    fix: 메모 목록 화면을 Compose로 구현 #27

--- presentation/src/main/java/com/lunchplay/ui/memo/MemoActivity.kt
+++ presentation/src/main/java/com/lunchplay/ui/memo/MemoActivity.kt
@@ -2,29 +2,16 @@ package com.lunchplay.ui.memo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import com.lunchplay.ui.R
-import com.lunchplay.ui.databinding.ActivityMemoBinding
-import com.lunchplay.ui.memo.fragment.MemoListFragment
+import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MemoActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMemoBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setBinding()
-        setFragment()
-    }
 
-    private fun setBinding() {
-        binding = DataBindingUtil.inflate(layoutInflater, R.layout.activity_memo, null, false)
-        binding.lifecycleOwner = this
-        setContentView(binding.root)
-    }
-
-    private fun setFragment() {
-        supportFragmentManager.beginTransaction().add(binding.navHostFragment.id, MemoListFragment())
+        setContent {
+            MemoListScreen()
+        }
     }
 }
---
+++ presentation/src/main/java/com/lunchplay/ui/memo/MemoListScreen.kt
@@ -0,0 +1,159 @@
+package com.lunchplay.ui.memo
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Bottom
+import androidx.compose.foundation.layout.Arrangement.Center
+import androidx.compose.foundation.layout.Arrangement.SpaceBetween
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.End
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.lunchplay.ui.R
+import com.lunchplay.ui.memo.model.MemoListUiState
+import com.lunchplay.ui.memo.model.MemoUiModel
+import com.lunchplay.ui.memo.model.Period
+import com.lunchplay.ui.memo.model.WrittenTime
+import java.time.LocalDateTime
+
+@Composable
+fun MemoListScreen() {
+    val viewModel: MemoViewModel = viewModel()
+
+    val memoListUiState by viewModel.memoListUiState.collectAsState(initial = MemoListUiState.Loading)
+    when (memoListUiState) {
+        is MemoListUiState.Success -> {
+            ShowMemoList(memoListUiState = memoListUiState)
+        }
+        is MemoListUiState.Empty -> {
+            ShowTextAtCenter(R.string.info_new_memo)
+        }
+        is MemoListUiState.Error -> {
+            ShowTextAtCenter(R.string.info_memo_error)
+        }
+        is MemoListUiState.Loading -> Unit
+    }
+
+    ShowMemoCreateButton {
+        //Navigate to CreateScreen
+    }
+}
+
+@Composable
+fun ShowMemoList(memoListUiState: MemoListUiState) {
+    LazyColumn(Modifier.fillMaxSize()) {
+        val data = (memoListUiState as MemoListUiState.Success).memos
+        items(data) { item ->
+            MemoListItem(item) {
+                //Navigate to DetailScreen
+            }
+        }
+    }
+}
+
+@Composable
+fun ShowTextAtCenter(resId: Int) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Center,
+        horizontalAlignment = CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = resId),
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Composable
+fun ShowMemoCreateButton(onclick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(30.dp),
+        verticalArrangement = Bottom,
+        horizontalAlignment = End,
+    ) {
+        FloatingActionButton(onClick = onclick) {
+            Icon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = stringResource(id = R.string.create_memo)
+            )
+        }
+    }
+}
+
+@Composable
+fun MemoListItem(
+    item: MemoUiModel,
+    onclick: () -> Unit
+) {
+    val dateText = when (item.date.period) {
+        Period.FEW_YEARS_AGO -> stringResource(
+            id = R.string.memo_few_years_ago,
+            item.date.dateTime.year,
+            item.date.dateTime.monthValue,
+            item.date.dateTime.dayOfMonth
+        )
+        Period.THIS_YEAR -> stringResource(
+            id = R.string.memo_this_year,
+            item.date.dateTime.monthValue,
+            item.date.dateTime.dayOfMonth
+        )
+        Period.TODAY -> stringResource(
+            id = R.string.memo_today,
+            item.date.dateTime.hour,
+            item.date.dateTime.minute,
+        )
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onclick)
+            .padding(20.dp),
+        horizontalArrangement = SpaceBetween,
+    ) {
+        Text(
+            text = item.title,
+            fontSize = 20.sp,
+        )
+        Text(
+            text = dateText,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMemoListItem(){
+    val sampleMemo = MemoUiModel(
+        0,
+        "제목",
+        "내용",
+        WrittenTime(
+            Period.TODAY,
+            LocalDateTime.now()
+        )
+    )
+    
+    MemoListItem(
+        item = sampleMemo,
+        onclick = { }
+    )
+}
